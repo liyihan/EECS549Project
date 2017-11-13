7a0528cd6ef5ff7c6136f983aa34cb51942e356e
Chang Liu <changliu@0587373104.wireless.umich.net>
2017-11-13 14:40:20 -0500

    Doc and Query file generated

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,12 +1,40 @@
-__author__ = 'liyihan'
+__author__ = 'changliu'
 
 def readDocsCran():
-    return []
+    f = open("dataSet/cran/cran.all.1400")
+    preprossedDoc = ""
+    index = 1
+
+    readText = False
+    for line in f.readlines():
+        if line[0] == '.' and line[1] == 'I':
+            preprossedDoc = preprossedDoc + str(index) + '\n'
+            index = index + 1
+            readText = False
+        elif line[0] == '.' and line[1] == 'W':
+            readText = True
+        elif readText:
+            preprossedDoc = preprossedDoc + line
+
+    return preprossedDoc
+
+def readDocsNpl():
+    f = open("dataSet/npl/doc-text")
+    preprossedDoc = ""
+    for line in f.readlines():
+        if line[0] == " ":
+            continue
+        else:
+            preprossedDoc = preprossedDoc + line
+
+    return preprossedDoc
 
 def readDocs(filename):
-    docs = []
     if filename == 'cran':
         return readDocsCran()
+    elif filename == 'npl':
+        return readDocsNpl()
+
     '''
     1
     doc 1 is this
@@ -18,8 +46,43 @@ def readDocs(filename):
     '''
     return docs
 
+
+def readQueriesCran():
+    f = open("dataSet/cran/cran.qry")
+    preprossedQuery = ""
+    index = 1
+
+    readText = False
+    for line in f.readlines():
+        if line[0] == '.' and line[1] == 'I':
+            preprossedQuery = preprossedQuery + str(index) + '\n'
+            index = index + 1
+            readText = False
+        elif line[0] == '.' and line[1] == 'W':
+            readText = True
+        elif readText:
+            preprossedQuery = preprossedQuery + line
+
+    return preprossedQuery
+
+
+def readQueriesNpl():
+    f = open("dataSet/npl/query-text")
+    preprossedQuery = ""
+    for line in f.readlines():
+        if line[0] == "/":
+            continue
+        else:
+            preprossedQuery = preprossedQuery + line
+
+    return preprossedQuery
+
 def readQueries(filename):
-    queries = []
+    if filename == 'cran':
+        return readQueriesCran()
+    elif filename == 'npl':
+        return readQueriesNpl()
+
     '''
     1
     query 1 is this
@@ -61,3 +124,17 @@ def xFoldValidation(x):
 
 
 cranDocs = readDocs('cran')
+cranDocsFile = open("cranDocs.txt", 'w')
+cranDocsFile.write(cranDocs)
+
+nplDocs = readDocs('npl')
+nplDocsFile = open("nplDocs.txt", 'w')
+nplDocsFile.write(nplDocs)
+
+cranQueries = readQueries('cran')
+cranQueriesFile = open("cranQueries.txt", 'w')
+cranQueriesFile.write(cranQueries)
+
+nplQuries = readQueries('npl')
+nplQuriesFile = open("nplQuries.txt", 'w')
+nplQuriesFile.write(nplQuries)
