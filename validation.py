9842e45c7d1acdfa9183ac5ff4132a87ad575bc9
Wenqi Dong <wqdong@umich.edu>
2017-11-27 21:17:24 -0500

    integrated

--- validation.py
+++ validation.py
@@ -23,10 +23,11 @@ import pdb
 def MAP(datasetName, fold):
 	# goldDir = 'dataSet/pDataSet/' + datasetName + '/' + datasetName + 'Test'
 	# ResultDir = 'dataSet/pDataSet/' + datasetName + '/' + datasetName + 'Otfidf'
-	relDir = datasetName + 'Rel'
+	relDir = datasetName + 'Rel.txt'
 	relDict = {}
 	rel = open(relDir,'r').read().split('\n')
-	for line in rel:
+	for line in rel[:-1]:
+		# print "line", line
 		[qid, docid, _] = line.split()
 		if qid in relDict.keys():
 			relDict[qid].append(docid)
@@ -38,13 +39,14 @@ def MAP(datasetName, fold):
 	MAPs = []
 	# for each fold
 	for i in range(fold):
-		resultFile = ResultDir + str(i+1) + '.txt'
-		goldFile = goldDir + str(i+1) + '.txt'
+		resultFile = ResultDir + str(i) + '.txt'
+		goldFile = goldDir + str(i) + '.txt'
 		result = open(resultFile, 'r').read().split('\n')
-		gold = open(goldFile, 'r').read().split('\n')
+		gold = open(goldFile, 'r').read()
 		goldid = gold.split()
 		result_dict = {}
-		for line in result:
+		for line in result[:-1]:
+			# print line
 			[qid, docid, _] = line.split()
 			if qid in result_dict.keys():
 				result_dict[qid].append(docid)
@@ -56,7 +58,7 @@ def MAP(datasetName, fold):
 			num, corr, avg = 0.0, 0.0, 0.0
 			for docid in res:
 				num += 1	
-				if docid in gold_dict[qid]:
+				if docid in relDict[qid]:
 					corr += 1
 				avg += corr/num
 			avg /= len(res)
@@ -64,4 +66,4 @@ def MAP(datasetName, fold):
 		MAPs.append(np.mean(avePs))
 	return np.mean(MAPs)
 
-print MAP('cran', 3)
+print MAP('npl', 5)
